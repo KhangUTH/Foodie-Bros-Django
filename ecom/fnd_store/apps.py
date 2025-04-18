28f4f2f97fe015f807f512c12e2e6221886e80b9
KhangUTH <khangtm2739@ut.edu.vn>
2025-04-18 20:42:48 +0700

    Next step

--- ecom/fnd_store/apps.py
+++ ecom/fnd_store/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class FndStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'fnd_store'
